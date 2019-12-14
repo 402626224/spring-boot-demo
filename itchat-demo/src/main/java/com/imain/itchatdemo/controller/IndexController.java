bc60268576a47bffbbc2556f64cfe3fad80def25
402626224 <402626224@qq.com>
2019-12-14 16:02:44 +0800

    修改 monitor

--- itchat-demo/src/main/java/com/imain/itchatdemo/controller/IndexController.java
+++ itchat-demo/src/main/java/com/imain/itchatdemo/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.imain.itchatdemo.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.imain.itchatdemo.model.ProductModel;
+import com.imain.itchatdemo.model.ResultInfoVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +9,23 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 /**
  * itchat4j: https://github.com/yaphone/itchat4j
  * author Songrui.Liu
  * date 2019/12/1317:35
  */
-@RestController
+@Controller
 @RequestMapping()
 public class IndexController {
 
@@ -38,35 +39,46 @@ public class IndexController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private HttpSession session;
+
     @RequestMapping("/")
-    public String index() throws Exception {
+    public String index(Model model) throws Exception {
+        Object paramVal = session.getAttribute("paramVal");
+        model.addAttribute("paramVal", paramVal == null ? "" : paramVal);
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/count")
+    public ResultInfoVo postAjax(String url, String param) {
         long time = System.currentTimeMillis();
-        //http://shop225.wqmeng.cn/app/index.php?i=21&c=entry&m=ewei_shopv2&do=mobile&r=goods.detail.get_comments&id=792&_=1576230047878
-        String url = "http://shop225.wqmeng.cn/app/index.php?i=21&c=entry&m=ewei_shopv2&do=mobile&r=goods.detail.get_comments&id=792&_=" + String.valueOf(time);
-        String userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1278.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.5 WindowsWechat";
-        List<String> cookies = new ArrayList<String>();
-        /* 登录获取Cookie 这里是直接给Cookie，可使用下方的login方法拿到Cookie给入*/
-        //Cookie: PHPSESSID=9c1d769548767af2ada999b8333ecf2a; PHPSESSID=9c1d769548767af2ada999b8333ecf2a
-        cookies.add("PHPSESSID=9c1d769548767af2ada999b8333ecf2a");       //在 header 中存入cookies
-        cookies.add("PHPSESSID=9c1d769548767af2ada999b8333ecf2a");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.put(HttpHeaders.COOKIE, cookies);        //将cookie存入头部
-        headers.set(HttpHeaders.USER_AGENT, userAgent);
-        HttpEntity<String> entity = new HttpEntity<>(url, headers);
-
-        ResponseEntity<String> obj = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        String resp = obj.getBody();
-
-        ProductModel productModel = new ObjectMapper().readValue(resp, ProductModel.class);
-
-        String all = Optional.ofNullable(productModel).map(p -> p.getResult()).map(r -> r.getCount()).map(c -> c.getAll()).orElse("0");
-        if (Objects.equals("0", all)) {
-            logger.info("all : 0");
+        String defaultUrl = "http://shop225.wqmeng.cn/app/index.php?i=21&c=entry&m=ewei_shopv2&do=mobile&r=goods.detail.get_comments&id=792&_=" + String.valueOf(time);
+        try {
+            if (StringUtils.isEmpty(url)) {
+                url = defaultUrl;
+            }
+            if (!StringUtils.isEmpty(param)) {
+                session.setAttribute("paramVal", param);
+            }
+            String paramVal = String.valueOf(session.getAttribute("paramVal"));
+            String userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1278.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.5 WindowsWechat";
+            List<String> cookies = new ArrayList<String>();
+            /* 登录获取Cookie 这里是直接给Cookie，可使用下方的login方法拿到Cookie给入*/
+            //Cookie: PHPSESSID=9c1d769548767af2ada999b8333ecf2a; PHPSESSID=9c1d769548767af2ada999b8333ecf2a
+            cookies.add("PHPSESSID=" + paramVal);       //在 header 中存入cookies
+            cookies.add("PHPSESSID=" + paramVal);
+            HttpHeaders headers = new HttpHeaders();
+            headers.put(HttpHeaders.COOKIE, cookies);        //将cookie存入头部
+            headers.set(HttpHeaders.USER_AGENT, userAgent);
+            HttpEntity<String> entity = new HttpEntity<>(url, headers);
+            ResponseEntity<String> obj = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            String resp = obj.getBody();
+            return new ResultInfoVo().setCode(200).setMsg(resp);
+        } catch (Exception e) {
+            logger.info("postAjax", e);
         }
-
-        logger.info(productModel.toString());
-        return resp;
+        return new ResultInfoVo().setCode(101).setMsg("请求失败");
     }
 
 
---
+++ itchat-demo/src/main/java/com/imain/itchatdemo/model/ResultInfoVo.java
@@ -0,0 +1,26 @@
+package com.imain.itchatdemo.model;
+
+public class ResultInfoVo {
+
+    private Integer code;
+
+    private  String msg;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public ResultInfoVo setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public ResultInfoVo setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+}
